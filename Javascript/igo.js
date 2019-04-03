56117dfb80817244963a3e00df12475761616570
Velhinho <goncalo.velhinho420@gmail.com>
2019-04-03 12:58:05 +0100

    added backbutton for weather screens

--- Javascript/igo.js
+++ Javascript/igo.js
@@ -1,9 +1,3 @@
-function showTime() {
-	var today = new Date();
-  var hours = today.getHours();
-  var mins = today.getMinutes();
-  if (mins < 10) {
-  	mins = "0" + mins;
-  }
-	document.getElementById("clock").innerHTML = hours + ":" + mins;
+function backbutton(){
+	window.history.back();
 }
